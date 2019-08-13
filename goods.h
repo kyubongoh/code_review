9476691fe7fe23da6fc4da729895095a81f4677d
kyubongoh <53921560+kyubongoh@users.noreply.github.com>
2019-08-13 15:28:27 +0900

    Remove unnecessary comments

--- goods.h
+++ goods.h
@@ -2,10 +2,6 @@
 #define GOODS_H_
 
 // Goods class for representing goods information with index and price
-// Example:
-//    Goods test;
-//    test.SetGoodsInfo(1, 10000);
-//    std::cout << test.GetIndex() << " - " << test.GetPrice << endl;
 class Goods{
  public:
   void SetGoodsInfo(int index, int price);
@@ -16,6 +12,5 @@ class Goods{
   int goods_index_ = 0;
   int goods_price_ = 0;
 };
-
 
 #endif  // GOODS_H_
